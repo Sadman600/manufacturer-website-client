8bce9e4e314f9bee47225ebeade067a93eeee493
Sadman600 <sadmansakib600@gmail.com>
2022-05-22 16:54:53 +0600

    Added some line in product component

--- src/Pages/Main/Home/Product.js
+++ src/Pages/Main/Home/Product.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { img, name, description, minimumOrder, availableQuantity, price } = product;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl my-3">
-            <figure><img src="https://www.goodwillaccss.com/products/bbelblack2.png" alt="Shoes" /></figure>
+            <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">Elastic Belt</h2>
-                <p>Our company holds vast experience in this domain and is involved in offering Garments Elastic Tape. </p>
-                <h4 className='font-bold'>Order Quantity: 3 (minimum)</h4>
-                <h4 className='font-bold'>Available Quantity: 13 pcs</h4>
-                <h4 className='font-bold text-xl'>Price: $10</h4>
+                <h2 className="card-title">{name}</h2>
+                <p>{description} </p>
+                <h4 className='font-bold'>Order Quantity: {minimumOrder} (minimum)</h4>
+                <h4 className='font-bold'>Available Quantity: {availableQuantity} pcs</h4>
+                <h4 className='font-bold text-xl'>Price: ${price}</h4>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary text-white font-bold">Buy Now</button>
                 </div>
--- src/Pages/Main/Home/Products.js
+++ src/Pages/Main/Home/Products.js
@@ -7,13 +7,13 @@ const Products = () => {
         fetch('accessories.json')
             .then(res => res.json())
             .then(data => setProduct(data))
-    }, []);
+    }, [products]);
     return (
         <div className='my-5 p-5'>
             <h1 className='text-center text-5xl text-primary font-bold'>Our Accessories</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3'>
                 {
-                    products.map((product, index) => <Product key={index}></Product>)
+                    products.map((product, index) => <Product key={index} product={product}></Product>)
                 }
             </div>
         </div>
